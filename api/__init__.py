fdf70fe3e610ca2316717afa3e16d9c7e71fe42e
Dimama <citrus_m@mail.ru>
2019-03-30 23:21:53 +0300

    configure CORS

--- api/__init__.py
+++ api/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask, current_app
 from flask_restful import Api
-
+from flask_cors import CORS
 from api.resources import Partner, User
 from db_wrapper import DBWrapper
 
@@ -8,8 +8,9 @@ from config import DATABASE, USER, PASSWORD, HOST, PORT, SCHEMA
 
 
 def create_app():
-
     app = Flask(__name__)
+
+    cors = CORS(app, resources={r"*": {"origins": "*"}})
 
     with app.app_context():
         current_app.db_wrapper = DBWrapper(DATABASE, USER, PASSWORD, HOST, PORT, SCHEMA)
