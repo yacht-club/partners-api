f0faaa30efd5c2c2e04ed361794a1659f893bae7
Dimama <citrus_m@mail.ru>
2019-03-30 15:02:18 +0300

    add db credentials

--- config.py
+++ config.py
@@ -1,1 +1,7 @@
 PORT = 8080
+
+jclassname = 'org.postgresql.Driver'
+driver_args = ['jdbc:postgresql://ec2-54-246-121-32.eu-west-1.compute.amazonaws.com/de1i7r1tk47278?sslmode=require&amp;sslfactory=org.postgresql.ssl.NonValidatingFactory',
+               'zhjidbjtviiygs',
+               '7f014fcf757bfb60fc49b8dd9f924bc3857dc98513e2efb3ae38cf3b064287e9']
+jar_path = "path-to/postgresql-x.x.x.jar"
